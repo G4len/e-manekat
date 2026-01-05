ede7dbd325ebefcc2a7adc7394f0bde722380b42
G4len <73323546+G4len@users.noreply.github.com>
2026-01-05 14:51:57 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1600, // Menambah limit menjadi 1.6 MB
+  },
 })
