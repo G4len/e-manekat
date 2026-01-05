5269c816cfd019e45e85dc302bdbda5fb0adc98e
G4len <73323546+G4len@users.noreply.github.com>
2026-01-05 14:53:31 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,9 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            // Memisahkan Firebase menjadi chunk tersendiri
-            if (id.includes('firebase')) {
-              return 'firebase-vendor';
-            }
-            // Memisahkan library lainnya
-            return 'vendor';
-          }
-        },
-      },
-    },
+    chunkSizeWarningLimit: 1600, // Menambah limit menjadi 1.6 MB
   },
 })
